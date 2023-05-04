ee9baea93521c2acdc22049a31a92320be8ac395
kajal-2011 <kajalrekha888@gmail.com>
2023-05-05 00:45:34 +0600

    courses api done

---
+++ pages/api/courses/[courseId].js
@@ -0,0 +1,12 @@
+import { getCourse } from "@/prisma/courses";
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === "GET") {
+      const course = await getCourse(req.query.courseId);
+      return res.status(200).json(course);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
