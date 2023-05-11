eaf8869e15fe59f5a15d8c64d790ae46d1ebca2f
kajal-2011 <kajalrekha888@gmail.com>
2023-05-11 23:49:48 +0600

    courses added in home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-const HomePage = () => {
-  return (
-    <div>
-      <h2 className="text-8xl">Home Page</h2>
-    </div>
-  );
+import { getAllCourses } from "@/prisma/courses";
+import CoursesPage from "./courses";
+
+const HomePage = ({ courses }) => {
+  return <CoursesPage courses={courses} />;
 };
 
 export default HomePage;
+
+export const getServerSideProps = async () => {
+  const courses = await getAllCourses();
+
+  const updatedCourses = courses.map((course) => ({
+    ...course,
+    updatedAt: course.updatedAt.toString(),
+    createdAt: course.createdAt.toString(),
+  }));
+
+  return {
+    props: {
+      courses: updatedCourses,
+    },
+  };
+};
