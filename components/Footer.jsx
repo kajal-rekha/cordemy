c5f8115ff3a201aefe3c140bb9a3d82b982ea2fa
kajal-2011 <kajalrekha888@gmail.com>
2023-05-12 00:52:54 +0600

    footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-black text-gray-400 text-center py-10">
-      <p>&copy; {new Date().getFullYear()} Codemastery. All rights reserved</p>
+      <p>&copy; {new Date().getFullYear()} Cordemy. All rights reserved</p>
     </div>
   );
 };
