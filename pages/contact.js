544db20c9b05cb81b4aff897f747aae310052d92
kajal-2011 <kajalrekha888@gmail.com>
2023-05-13 14:01:05 +0600

    contact form update

--- pages/contact.js
+++ pages/contact.js
@@ -4,12 +4,11 @@ const ContactPage = () => {
   };
   return (
     <div className=" contact container mx-auto mt-20 h-screen">
+      <h2 className="text-5xl text-center font-medium  ">Contact Us</h2>
       <form
         onSubmit={sendEmail}
         className="contact-form flex flex-col gap-5 py-10 mx-auto max-w-lg"
       >
-        <h2 className="text-5xl font-medium  ">Contact Us</h2>
-
         <div className="form-control flex flex-col gap-2">
           <label htmlFor="name" className="cursor-pointer">
             Name
@@ -18,7 +17,7 @@ const ContactPage = () => {
             type="name"
             name="name"
             placeholder="write your name"
-            className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none "
+            className="border border-gray-300 focus:border-gray-600 py-3 px-5 rounded-xl outline-none duration-300 "
           />
         </div>
         <div className="form-control flex flex-col gap-2">
@@ -29,24 +28,26 @@ const ContactPage = () => {
             type="email"
             name="email"
             placeholder="write your email"
-            className=" border border-slate-500 py-3 px-5 rounded-xl outline-none"
+            className=" border border-gray-300 focus:border-gray-600 py-3 px-5 rounded-xl outline-none duration-300"
           />
         </div>
 
         <div className="form-control flex flex-col gap-2">
-          <label htmlFor="message" className="cursor-pointer ">
+          <label htmlFor="message" className="cursor-pointer">
             Message
           </label>
           <textarea
             placeholder="Write your message"
             name="message"
-            required
             rows="1"
             cols="30"
-            className="message border border-slate-500 py-16 px-28 rounded-xl outline-none "
+            className="message border border-gray-300 focus:border-gray-600 h-40 rounded-xl outline-none py-5 px-5 resize-none duration-300"
           />
         </div>
-        <input type="submit" className="px-2 py-5 bg-black text-white" />
+        <input
+          type="submit"
+          className="px-2 py-5 bg-black text-white rounded-xl"
+        />
       </form>
     </div>
   );
