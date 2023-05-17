96345747b07fe82b1d0f87d9432e4c79f2d31045
kajal-2011 <kajalrekha888@gmail.com>
2023-05-17 21:53:32 +0600

    error solved

--- pages/users/login.jsx
+++ pages/users/login.jsx
@@ -1,5 +1,61 @@
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+import SectionHeader from "@/components/SectionHeader";
+import { FcGoogle } from "react-icons/fc";
+import { getSession, signIn } from "next-auth/react";
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const LoginPage = ({ session }) => {
+  const loginWithGoogle = async () => {
+    try {
+      await signIn("google");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="wrapper py-10 min-h-screen">
+      <SectionHeader
+        span={"Login"}
+        h2={"Get started with Google"}
+        p={"Please login to continue with our features!"}
+      />
+      <div className="flex justify-center">
+        <button
+          onClick={loginWithGoogle}
+          className="flex gap-2 items-center bg-black text-white py-2 px-6 rounded-lg mt-8 hover:bg-gray-700 duration-300"
+        >
+          {" "}
+          <span>
+            <FcGoogle />
+          </span>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    const destination = context.query.destination || "/users/profile";
+
+    return {
+      redirect: {
+        destination,
+        performance: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
