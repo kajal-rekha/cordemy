d30c40ac1ef187998d247f6a89300dac0ef53534
kajal-2011 <kajalrekha888@gmail.com>
2023-06-03 17:59:31 +0600

    hero section added

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Button from "./Button";
+import Overlay from "./Overlay";
+
 const Hero = () => {
   return (
     <div className=" min-h-screen">
@@ -7,6 +11,24 @@ const Hero = () => {
         }}
         className="w-full h-[32rem] bg-no-repeat bg-cover bg-center"
       />
+      <Overlay />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center z-[2] space-y-5">
+        <h1 className="text-5xl font-semibold">
+          Unlock Your Potential with Online Learning
+        </h1>
+        <p className="text-lg">
+          Learn new skills and enhance your knowledge with our wide range of
+          online courses.
+        </p>
+        <div className="mt-5  ">
+          <Link
+            href="/users/login"
+            className="bg-black text-white py-3 px-6 rounded-lg hover:bg-gray-700 duration-300"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/Overlay.jsx
@@ -0,0 +1,7 @@
+const Overlay = () => {
+  return (
+    <div className="absolute top-0 left-0 z-[1] w-full h-full bg-gradient-to-t from-black/50"></div>
+  );
+};
+
+export default Overlay;
