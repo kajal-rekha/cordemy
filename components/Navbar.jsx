5baad854c1ef497375d31d640599d09b26c9831d
kajal-2011 <kajalrekha888@gmail.com>
2023-06-11 22:51:05 +0600

    nav toggle added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,69 +1,174 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 import Button from "./Button";
-import { motion } from "framer-motion";
-import { getTransition, shutterDown } from "@/utils/motion";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+  const handleOpen = () => {
+    setToggle(false);
+  };
+
   const { data: session } = useSession();
+
   return (
-    <div className="h-20 bg-black text-gray-400 flex items-center">
-      <div className="wrapper flex justify-between">
-        <motion.div
-          variants={shutterDown()}
-          initial="from"
-          animate="to"
-          transition={getTransition()}
-        >
-          <Link href="/" className="text-white font-semibold">
-            Cordemy
-          </Link>
-        </motion.div>
-
-        <div className="flex gap-5">
-          <Link href="/" className="hover:text-white transition-color">
-            Home
-          </Link>
-          <Link href="/courses" className="hover:text-white transition-color">
-            Courses
-          </Link>
-          {session && (
-            <Link href="/orders" className="hover:text-white transition-color">
-              Orders
+    <div>
+      <div className="h-20 bg-black text-gray-400 flex items-center">
+        <div className="wrapper flex justify-between items-center">
+          <div className="logo" data-aos="fade-right" data-aos-duration="1000">
+            <Link href="/" className="text-white font-semibold">
+              Cordemy
             </Link>
-          )}
+          </div>
 
-          <Link
-            href="/testimonials"
-            className="hover:text-white transition-color"
+          <div className="hidden md:block">
+            <div
+              data-aos="fade-down"
+              data-aos-duration="1000"
+              className="flex gap-5 justify-between"
+            >
+              <Link href="/" className="hover:text-white transition-color">
+                Home
+              </Link>
+              <Link
+                href="/courses"
+                className="hover:text-white transition-color"
+              >
+                Courses
+              </Link>
+              {session && (
+                <Link
+                  href="/orders"
+                  className="hover:text-white transition-color"
+                >
+                  Orders
+                </Link>
+              )}
+              <Link
+                href="/testimonials"
+                className="hover:text-white transition-color"
+              >
+                Testimonials
+              </Link>
+
+              <Link href="/about" className="hover:text-white transition-color">
+                About
+              </Link>
+
+              <Link
+                href="/contact"
+                className="hover:text-white transition-color"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+          <div
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            className="hidden md:block "
           >
-            Testimonials
-          </Link>
+            {!session ? (
+              <Button
+                href="/users/login"
+                placeholder="Sign in"
+                color="secondary"
+              />
+            ) : (
+              <Button
+                href="/users/profile"
+                placeholder="profile"
+                color="secondary"
+              />
+            )}
+          </div>
 
-          <Link href="/about" className="hover:text-white transition-color">
-            About
-          </Link>
-
-          <Link href="/contact" className="hover:text-white transition-color">
-            Contact
-          </Link>
+          {/*toggle button */}
+          <div className="flex md:hidden">
+            <button
+              type="button"
+              onClick={handleToggle}
+              className="inline-flex items-center justify-center focus:outline-none text-xl text-white z-[99]"
+            >
+              {toggle === true ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
         </div>
+      </div>
 
-        <div>
-          {!session ? (
-            <Button
-              href="/users/login"
-              placeholder="Sign in"
-              color="secondary"
-            />
-          ) : (
-            <Button
-              href="/users/profile"
-              placeholder="Profile"
-              color="secondary"
-            />
-          )}
-        </div>
+      {/* mobile menu */}
+      <div>
+        {toggle ? (
+          <div className="md:hidden">
+            <div
+              onClick={handleOpen}
+              className={`w-screen h-screen fixed z-[98] bg-black/50 top-0 left-0 right-0 bottom-0 flex text-white justify-center items-center ${
+                setToggle ? `` : "hidden"
+              }`}
+            >
+              <div>
+                <div className="flex flex-col gap-5 items-center mb-10">
+                  <Link href="/" className="hover:text-white transition-color">
+                    Home
+                  </Link>
+                  <Link
+                    href="/courses"
+                    className="hover:text-white transition-color"
+                  >
+                    Courses
+                  </Link>
+                  {session && (
+                    <Link
+                      href="/orders"
+                      className="hover:text-white transition-color"
+                    >
+                      Orders
+                    </Link>
+                  )}
+
+                  <Link
+                    href="/testimonials"
+                    className="hover:text-white transition-color"
+                  >
+                    Testimonials
+                  </Link>
+                  <Link
+                    href="/about"
+                    className="hover:text-white transition-color"
+                  >
+                    About
+                  </Link>
+
+                  <Link
+                    href="/contact"
+                    className="hover:text-white transition-color"
+                  >
+                    Contact
+                  </Link>
+                </div>
+
+                {!session ? (
+                  <Button
+                    href="/users/login"
+                    placeholder="Sign in"
+                    color="secondary"
+                  />
+                ) : (
+                  <Button
+                    href="/users/profile"
+                    placeholder="profile"
+                    color="secondary"
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
